772deea866b40b2d9c696599674f089cab9eef19
CrisMystik <62919067+CrisMystik@users.noreply.github.com>
2025-02-26 15:10:00 +0100

    Type-hint dispatcher.groups

--- pyrogram/dispatcher.py
+++ pyrogram/dispatcher.py
@@ -20,6 +20,7 @@ import asyncio
 import inspect
 import logging
 from collections import OrderedDict
+from typing import Dict, List
 
 import pyrogram
 from pyrogram import utils
@@ -29,6 +30,7 @@ from pyrogram.handlers import (
     ChosenInlineResultHandler, ChatMemberUpdatedHandler, ChatJoinRequestHandler, StoryHandler,
     ShippingQueryHandler, MessageReactionHandler, MessageReactionCountHandler, ChatBoostHandler, PurchasedPaidMediaHandler
 )
+from pyrogram.handlers.handler import Handler
 from pyrogram.raw.types import (
     UpdateNewMessage, UpdateNewChannelMessage, UpdateNewScheduledMessage,
     UpdateBotNewBusinessMessage, UpdateBotEditBusinessMessage, UpdateBotDeleteBusinessMessage,
@@ -72,7 +74,7 @@ class Dispatcher:
         self.locks_list = []
 
         self.updates_queue = asyncio.Queue()
-        self.groups = OrderedDict()
+        self.groups: Dict[int, List[Handler]] = OrderedDict()
 
         async def message_parser(update, users, chats):
             connection_id = getattr(update, "connection_id", None)
