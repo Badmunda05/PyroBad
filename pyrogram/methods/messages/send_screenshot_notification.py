002f9ac058e5c1a9afcabebd03e77b30db87953a
Mahdi K. <106227973+m2k7m@users.noreply.github.com>
2025-04-02 14:18:28 +0300

    chat_id

--- pyrogram/methods/messages/send_screenshot_notification.py
+++ pyrogram/methods/messages/send_screenshot_notification.py
@@ -24,7 +24,7 @@ from pyrogram import raw, types, utils
 class SendScreenshotNotification:
     async def send_screenshot_notification(
         self: "pyrogram.Client",
-        user_id: Union[int, str],
+        chat_id: Union[int, str],
         reply_parameters: "types.ReplyParameters" = None
     ) -> "types.Message":
         """Notify the other user in a private chat that a screenshot of the chat was taken.
@@ -32,8 +32,9 @@ class SendScreenshotNotification:
         .. include:: /_includes/usable-by/users.rst
 
         Parameters:
-            user_id (``int`` | ``str``):
-                Unique identifier (int) or username (str) of the target user,
+            chat_id (``int`` | ``str``):
+                Unique identifier (int) or username (str) of the target chat.
+                For your personal cloud (Saved Messages) you can simply use "me" or "self".
                 For a contact that exists in your Telegram address book you can use his phone number (str).
 
             reply_parameters (:obj:`~pyrogram.types.ReplyParameters`, *optional*):
@@ -45,7 +46,7 @@ class SendScreenshotNotification:
 
         r = await self.invoke(
             raw.functions.messages.SendScreenshotNotification(
-                peer=await self.resolve_peer(user_id),
+                peer=await self.resolve_peer(chat_id),
                 reply_to=await utils.get_reply_to(self, reply_parameters) if reply_parameters else raw.types.InputReplyToMessage(reply_to_msg_id=0),
                 random_id=self.rnd_id()
             )
