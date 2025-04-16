8df3c382a4c7c150bd659f49b14ba7fd64816657
penggrin12 <miner.sidor@gmail.com>
2025-04-16 03:51:28 +0300

    Fix Python 3.11- support for generating raw exports

--- compiler/api/compiler.py
+++ compiler/api/compiler.py
@@ -593,7 +593,7 @@ def start(format: bool = False):
             if not namespace:
                 f.write(f"from . import {', '.join(filter(bool, namespaces_to_types))}")
             
-            f.write(f"\n__all__ = [{', '.join(f'\"{t}\"' for t in types)}]\n")
+            f.write("\n__all__ = [{}]\n".format(", ".join(f'"{t}"' for t in types)))
 
     for namespace, types in namespaces_to_constructors.items():
         with open(DESTINATION_PATH / "types" / namespace / "__init__.py", "w") as f:
@@ -611,7 +611,7 @@ def start(format: bool = False):
             if not namespace:
                 f.write(f"from . import {', '.join(filter(bool, namespaces_to_constructors))}\n")
 
-            f.write(f"\n__all__ = [{', '.join(f'\"{t}\"' for t in types)}]\n")
+            f.write("\n__all__ = [{}]\n".format(", ".join(f'"{t}"' for t in types)))
 
     for namespace, types in namespaces_to_functions.items():
         with open(DESTINATION_PATH / "functions" / namespace / "__init__.py", "w") as f:
@@ -629,7 +629,7 @@ def start(format: bool = False):
             if not namespace:
                 f.write(f"from . import {', '.join(filter(bool, namespaces_to_functions))}")
 
-            f.write(f"\n__all__ = [{', '.join(f'\"{t}\"' for t in types)}]\n")
+            f.write("\n__all__ = [{}]\n".format(", ".join(f'"{t}"' for t in types)))
 
     with open(DESTINATION_PATH / "all.py", "w", encoding="utf-8") as f:
         f.write(notice + "\n\n")
