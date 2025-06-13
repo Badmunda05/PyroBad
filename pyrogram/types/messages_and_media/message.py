4cc111bfc8eca918bceec92d3d545620428119c9
RimMirK <me@rimmirk.pp.ua>
2025-06-13 14:33:17 +0200

    Fix docstring in Message.delete()

--- pyrogram/types/messages_and_media/message.py
+++ pyrogram/types/messages_and_media/message.py
@@ -5436,16 +5436,16 @@ class Message(Object, Update):
                 Defaults to True.
 
         Returns:
-            True on success, False otherwise.
+            `int` – Amount of affected messages.
 
         Raises:
             RPCError: In case of a Telegram RPC error.
         """
-        return bool(await self._client.delete_messages(
+        return await self._client.delete_messages(
             chat_id=self.chat.id,
             message_ids=self.id,
             revoke=revoke
-        ))
+        )
 
     async def click(
         self,
