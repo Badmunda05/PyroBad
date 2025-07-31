13a797ceba81f421518d17bf9068de73058668d4
रोहित यादव <84123733+Itsydv@users.noreply.github.com>
2025-07-31 13:33:40 +0530

    Update verification_status.py
    Fixed AttributeError: 'NoneType' object has no attribute 'icon', when parsing ChatInvite

--- pyrogram/types/user_and_chats/verification_status.py
+++ pyrogram/types/user_and_chats/verification_status.py
@@ -63,7 +63,7 @@ class VerificationStatus(Object):
         bot_verification_icon = None
 
         if isinstance(chat, raw.types.ChatInvite):
-            bot_verification_icon = chat.bot_verification.icon
+            bot_verification_icon = chat.bot_verification.icon if chat.bot_verification else None
         else:
             bot_verification_icon = chat.bot_verification_icon
 
