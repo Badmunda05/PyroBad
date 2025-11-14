bdd04d7b816dd9912528fd19419966ab09cad7b5
madrik1337 <116110543+madrik1337@users.noreply.github.com>
2025-11-14 20:05:17 +0300

    delete allow_paid_floodskip

--- pyrogram/methods/messages/send_document.py
+++ pyrogram/methods/messages/send_document.py
@@ -295,7 +295,6 @@ class SendDocument:
                             random_id=self.rnd_id(),
                             schedule_date=utils.datetime_to_timestamp(schedule_date),
                             noforwards=protect_content,
-                            allow_paid_floodskip=allow_paid_broadcast,
                             reply_markup=await reply_markup.write(self) if reply_markup else None,
                             effect=effect_id,
                             allow_paid_stars=paid_message_star_count,
