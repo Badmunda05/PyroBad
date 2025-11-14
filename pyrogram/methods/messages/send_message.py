dfa3bc1b113adcfce3d7c1b4b059a383728c85b4
madrik1337 <116110543+madrik1337@users.noreply.github.com>
2025-11-14 20:02:30 +0300

    delete allow_paid_floodskip

--- pyrogram/methods/messages/send_message.py
+++ pyrogram/methods/messages/send_message.py
@@ -267,7 +267,6 @@ class SendMessage:
                 ),
                 random_id=self.rnd_id(),
                 schedule_date=utils.datetime_to_timestamp(schedule_date),
-                allow_paid_floodskip=allow_paid_broadcast,
                 allow_paid_stars=paid_message_star_count,
                 suggested_post=suggested_post_parameters.write() if suggested_post_parameters else None,
                 reply_markup=await reply_markup.write(self) if reply_markup else None,
